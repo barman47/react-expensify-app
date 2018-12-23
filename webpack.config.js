804cb1582ec40087e5db77e0cad9fb49997fc836
barman47 <nomsouzoanya@yahoo.co.uk>
2018-12-23 03:29:56 +0100

    Added Engines

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = (env) => {
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': json.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
